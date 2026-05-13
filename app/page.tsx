8d835ee38965d73dc8a88216d8060fa924fd34a9
Haindz <hafidzindra54@gmail.com>
2026-05-13 13:25:11 +0700

    Menambahkan teks.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1 className='text-4xl font-bold'>Halo, ini my project vercel saya!</h1>
       <p className='mt-4'>Berhasil di-deploy lewat git.</p>
+      <p className='mt-4'>When Yh...</p>
     </main>
   );
 }
